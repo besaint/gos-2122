1d7a0e24205d7516c558539a7e97b2fcbd32a61b
Viktor Zernov <viktorzernov4@gmail.com>
2022-01-27 19:09:22 +0300

    docker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const port = 3000
+const port = 8080
 
 const DICTIONARY = JSON.parse(fs.readFileSync('dictionary.json', 'utf8'))
 const DICTIONARY_GROUPED = group_by_letter(DICTIONARY);
